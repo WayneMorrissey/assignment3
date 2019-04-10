a35db9e7791c70026ede2e8b970f587fd716b85b
Wayne Morrissey <waynemorrissey96@gmail.com>
2019-04-09 23:44:38 -0600

    finito

--- dist/js/menu/menu.js
+++ dist/js/menu/menu.js
@@ -13,22 +13,31 @@ window.addEventListener("DOMContentLoaded", (event) => {
 //  code to animate mobile navigation menu.
   menuOpenButton.onclick = (e)=>{
       console.log("menu");
-     isOpen = !isOpen;
-     if(!isOpen){
-        TweenMax.to(menu,  0.8, {top:"-20rem",    ease:Power4.easeOut});
-     }else{
-        TweenMax.to(menu, 0.5,  {top:"4.5rem",   ease:Power4.easeOut});
-     }
-  }
+      isOpen = !isOpen;
+      if(!isOpen){
+         TweenMax.to(menu,  0.8, {top:"-20rem",    ease:Power4.easeOut, onComplete:hide});
+      }else{
+         show();
+         TweenMax.to(menu, 0.5,  {top:"4.5rem",   ease:Power4.easeOut});
+      }
+   }
  
   menu.onclick = (e) => {
-   isOpen = !isOpen;
-   if(!isOpen){
-      TweenMax.to(menu,  0.8, {top:"-20rem",    ease:Power4.easeOut});
-   }else{
-      TweenMax.to(menu, 0.5,  {top:"4.5rem",   ease:Power4.easeOut});
+      isOpen = !isOpen;
+      if(!isOpen){
+         TweenMax.to(menu,  0.8, {top:"-20rem",    ease:Power4.easeOut, onComplete:hide});
+      }else{
+         show();
+         TweenMax.to(menu, 0.5,  {top:"4.5rem",   ease:Power4.easeOut});
+      }
    }
-  }
  
+   const hide = () => {
+      menu.style.display = "none";
+   }
+
+   const show = () => {
+      menu.style.display = "block";
+   }
 });
  
